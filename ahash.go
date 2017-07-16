8e502f6c51b074c15b60cc94a1ba2c62accb2d00
devedge <devedge@outlook.com>
2017-07-16 15:56:02 -0400

    Average hash algorithm written

--- ahash.go
+++ ahash.go
@@ -7,16 +7,50 @@ Average hash algorithm
 package imagehash
 
 import (
-  "fmt"
-  "encoding/hex"
+  "image"
   "github.com/disintegration/imaging"
 )
 
-func AHash(img image.Image) {
-  res := imaging.Grayscale(img)
-  res = imaging.Resize(res, 8, 8, imaging.Lanczos)
 
-  // As the average value is being computed, create an array
+// Ahash calculates the average hash of an image. The image is first grayscaled,
+// then scaled down to "hashLen" for width and height. Then, the average value
+// of the pixels is computed, and if pixel is above the average, a 1 is appended
+// to the byte array; a 0 otherwise.
+func Ahash(img image.Image, hashLen int) ([]byte, error) {
+  var sum uint32                        // Sum of the pixels
+  numbits := hashLen * hashLen          // Perform the hashLen^2 operation once
+  bitArray,err := NewBitArray(numbits)  // Resultant byte array init
+  if err != nil { return nil, err }
+
+  // As the average is being computed, create & populate an array
   // of pixels to optimize runtime
   var pixelArray []uint32
+
+  // Grayscale and resize
+  res := imaging.Grayscale(img)
+  res = imaging.Resize(res, hashLen, hashLen, imaging.Lanczos)
+
+  // Iterate over every pixel to generate the sum.
+  // Additionally, store every pixel into an array for faster re-computation
+  for x := 0; x < 8; x++ {
+    for y := 0; y < 8; y++ {
+      r,_,_,_ := res.At(x,y).RGBA()       // r = g = b since the image is grayscaled
+      sum += r                            // increment the sum
+      pixelArray = append(pixelArray, r)  // append the pixel
+    }
+  }
+
+  // Compute the average
+  avg := sum / uint32(numbits)
+
+  // For every pixel, check if it's below or above the average
+  for _,pix := range pixelArray {
+    if pix > avg {
+      bitArray.AppendBit(1) // If above, append 1
+    } else {
+      bitArray.AppendBit(0) // else append 0
+    }
+  }
+
+  return bitArray.GetArray(), nil
 }
